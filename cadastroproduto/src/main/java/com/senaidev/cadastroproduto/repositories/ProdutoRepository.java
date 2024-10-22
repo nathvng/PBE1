d4ba13e34c245cbd261d42c1146072871cb91526
Nathália Venega <159180447+nathvng@users.noreply.github.com>
2024-10-22 13:47:03 -0300

    Delete cadastroproduto directory

--- cadastroproduto/src/main/java/com/senaidev/cadastroproduto/repositories/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senaidev.cadastroproduto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senaidev.cadastroproduto.entities.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-
-}
